63a6f99c79d1f204fa273418bf6e525705825785
mikewanjero <mikewanj@gmail.com>
2025-01-08 11:35:07 +0300

    outputting dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Michael", "John"];
+
 export default function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <h2>Michael</h2>
+      <h2>{chosenName}</h2>
       <p>Is on React Web now!</p>
     </div>
   );
