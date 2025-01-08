d325bdc7511ddc26e5f414f6672910c7ad2fa481
mikewanjero <mikewanj@gmail.com>
2025-01-08 16:12:55 +0300

    CSS Styling and CSS Modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
+import styles from "./Post.module.css";
+
 export default function Post(props) {
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.body}</p>
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
     </div>
   );
 }
