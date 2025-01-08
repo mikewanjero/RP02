d021627c4e962e515e8945bc1b2ee1df08edcee1
mikewanjero <mikewanj@gmail.com>
2025-01-08 14:27:45 +0300

    Reusing components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import "./App.css";
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
 }
 export default App;
