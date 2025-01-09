2e22f618e64eb1fad42c2cec6c74b9eed6bc5af5
mikewanjero <mikewanj@gmail.com>
2025-01-09 11:57:22 +0300

    Preparing the App for State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,15 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 
 export default function PostsList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Michael" body="Working with React.js" />
-      <Post author="John" body="Check out the full course" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Michael" body="Working with React.js" />
+        <Post author="John" body="Check out the full course" />
+      </ul>
+    </>
   );
 }
