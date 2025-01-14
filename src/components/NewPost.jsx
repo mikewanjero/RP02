eb47f83093fd14af3ed9e957f1ed471fe2eea7fe
mikewanjero <mikewanj@gmail.com>
2025-01-14 12:22:35 +0300

    Adding Form Buttons - Submit, Cancel

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,22 @@
 /* eslint-disable react/prop-types */
 import classes from "./NewPost.module.css";
 
-function NewPost(props) {
+function NewPost({ onStateChange, onAuthorChange, onCancel }) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onStateChange} />
+        <textarea id="body" required rows={3} onChange={onStateChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={onAuthorChange} />
+      </p>
+      <p className={classes.actions}>
+        <button type="submit">Submit</button>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
       </p>
     </form>
   );
